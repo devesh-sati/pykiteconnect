1ca481db8642cf63c92a763aade92cba7e053657
h-vishal <vhem4s@gmail.com>
2018-04-23 11:30:02 +0530

    Changed pip download location
    Point to https://github.com/h-vishal/pykiteconnect

--- kiteconnect/__version__.py
+++ kiteconnect/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "kiteconnect"
 __description__ = "The official Python client for the Kite Connect trading API"
 __url__ = "https://kite.trade"
-__download_url__ = "https://github.com/zerodhatech/pykiteconnect"
+__download_url__ = "https://github.com/devesh-sati/pykiteconnect"
 __version__ = "5.0.0"
 __author__ = "Zerodha Technology Pvt. Ltd. (India)"
 __author_email__ = "talk@zerodha.tech"
